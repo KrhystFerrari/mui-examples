15434013a3be93e0267e781d9491d0c7332c0551
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2023-08-05 12:32:54 -0300

    feat: create textField examples

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, TextField } from "@mui/material";
 
 function App() {
   return (
@@ -6,17 +6,39 @@ function App() {
       <Typography variant="h1" component={"h1"} sx={{ color: "blue" }}>
         MUI Examples
       </Typography>
-      <Button
-        variant="contained"
-        color="success"
-        sx={{ margin: "30px" }}
-      >
+      <Button variant="contained" color="success" sx={{ margin: "30px" }}>
         Click Me
       </Button>
-      <Button variant="text" disabled>Click Me</Button>
-      <Button variant="outlined" onClick={() => alert("Clicked !")} size="large">
+      <Button variant="text" disabled>
+        Click Me
+      </Button>
+      <Button
+        variant="outlined"
+        onClick={() => alert("Clicked !")}
+        size="large"
+      >
         Click Me for Action
       </Button>
+      <br />
+      <br />
+      <TextField
+        type="text"
+        placeholder="Name"
+        variant="standard"
+        sx={{ margin: "30px" }}
+      />
+      <TextField
+        type="text"
+        placeholder="Name"
+        variant="outlined"
+        sx={{ margin: "30px" }}
+      />
+      <TextField
+        type="text"
+        placeholder="Name"
+        variant="filled"
+        sx={{ margin: "30px" }}
+      />
     </div>
   );
 }
