c2b11140a6944bd84f431274d32c155c19e595b9
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2023-08-05 12:26:55 -0300

    feat: create button examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { Button, Typography } from "@mui/material";
 
 function App() {
   return (
     <div>
-      <h1>MUI Examples</h1>
+      <Typography variant="h1" component={"h1"} sx={{ color: "blue" }}>
+        MUI Examples
+      </Typography>
+      <Button
+        variant="contained"
+        color="success"
+        sx={{ margin: "30px" }}
+      >
+        Click Me
+      </Button>
+      <Button variant="text" disabled>Click Me</Button>
+      <Button variant="outlined" onClick={() => alert("Clicked !")} size="large">
+        Click Me for Action
+      </Button>
     </div>
   );
 }
