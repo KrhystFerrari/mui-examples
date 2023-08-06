47fb6c378d22290c004163c7f1302d068215f77f
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2023-08-06 12:03:22 -0300

    feat: apply theme with ThemeProvider

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff5733",
+    },
+  },
+  typography: {
+    button: {
+        width: '300px',
+        
+    }
+  }
+});
